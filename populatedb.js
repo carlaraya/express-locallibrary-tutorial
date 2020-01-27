62c6fe4760c6fb9aac549f19c7b6366304380929
carlaraya <carl.araya256@gmail.com>
2020-01-27 14:01:54 +0800

    Express Tutorial Part 6.3: Create Book form

--- populatedb.js
+++ populatedb.js
@@ -1,24 +1,17 @@
 #! /usr/bin/env node
 
-console.log('This script populates some test books, authors, genres and bookinstances to your database. Specified database as argument - e.g.: populatedb mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true');
+console.log('This script populates some test books, authors, genres and bookinstances to your database.');
 
-// Get arguments passed on command line
-var userArgs = process.argv.slice(2);
-/*
-if (!userArgs[0].startsWith('mongodb')) {
-    console.log('ERROR: You need to specify a valid mongodb URL as the first argument');
-    return
-}
-*/
 var async = require('async')
 var Book = require('./models/book')
 var Author = require('./models/author')
 var Genre = require('./models/genre')
 var BookInstance = require('./models/bookinstance')
 
+require('dotenv').config()
 
 var mongoose = require('mongoose');
-var mongoDB = userArgs[0];
+var mongoDB = process.env.MONGODBURI;
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
